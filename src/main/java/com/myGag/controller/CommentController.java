56db8c32078b6675cd8fe6204600d4dc2160860c
Katerina Petkova <katerina.petkowa@gmail.com>
2016-10-15 00:20:30 +0300

    write comment

--- src/main/java/com/myGag/controller/CommentController.java
+++ src/main/java/com/myGag/controller/CommentController.java
@@ -1,10 +1,29 @@
 package com.myGag.controller;
 
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.myGag.model.CommentsManager;
 
 @Controller
 public class CommentController {
 
+	@RequestMapping(value = "/uploadComment", method = RequestMethod.POST)
+	public String uploadComment(@RequestParam(value="username") String username, @RequestParam(value="comment") String comment,
+			HttpServletRequest request){
+		int postId = Integer.parseInt(request.getParameter("post_id"));
+		if(comment!=null){
+			CommentsManager.getInstance().uploadComment(username, postId, comment, 0 , LocalDateTime.now());
+			return "postDetails";
+		}
+		return "postDetails";
+	}
 	
 	
 	
